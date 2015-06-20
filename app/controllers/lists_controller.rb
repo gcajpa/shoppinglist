39b66e4a9a91a7e7b5b47deef996fc4749f66432
Gustavo Corrêa Alves <gcajpa@gmail.com>
2015-06-19 23:03:57 -0300

    ajust to send email

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -14,7 +14,7 @@ class ListsController < ApplicationController
 
   def create
     if @list.update_attributes(list_params)
-      Mailer.send_list("teste@teste.com", @list).deliver_later
+      Mailer.send_list("gcajpa@gmail.com", @list).deliver_later
       redirect_to @list
     else
       render 'new', notice: "Erro ao tentar salvar a lista"
